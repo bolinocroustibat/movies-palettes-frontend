60774e5a20a0735de7644bdfc3309f75f57f0565
Adrien Carpentier <me@adriencarpentier.com>
2024-11-30 16:07:05 +0100

    build: use bun adapter and lint code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,34 @@
-import scrollbar from 'tailwind-scrollbar';
-import type { Config } from 'tailwindcss';
+import scrollbar from "tailwind-scrollbar"
+import type { Config } from "tailwindcss"
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Work Sans', 'system-ui', 'sans-serif'],
-      },
-      fontSize: {
-        '4xl': ['2.5rem', {
-          lineHeight: '1.1',
-          letterSpacing: '-0.03em',
-          fontWeight: '600',
-        }],
-        '2xl': ['1.5rem', {
-          lineHeight: '1.2',
-          letterSpacing: '-0.02em',
-          fontWeight: '500',
-        }],
-      },
-    }
-  },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Work Sans", "system-ui", "sans-serif"],
+			},
+			fontSize: {
+				"4xl": [
+					"2.5rem",
+					{
+						lineHeight: "1.1",
+						letterSpacing: "-0.03em",
+						fontWeight: "600",
+					},
+				],
+				"2xl": [
+					"1.5rem",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "-0.02em",
+						fontWeight: "500",
+					},
+				],
+			},
+		},
+	},
 
-  plugins: [scrollbar({ nocompatible: true })]
-} satisfies Config;
+	plugins: [scrollbar({ nocompatible: true })],
+} satisfies Config
