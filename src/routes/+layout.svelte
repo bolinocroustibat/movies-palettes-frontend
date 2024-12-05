d66ef34c5d9864aa7b542669933406038c2e085b
Adrien Carpentier <me@adriencarpentier.com>
2024-12-05 23:58:58 +0100

    clean: remove non working debug and add env in CI build step

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
 import "../app.css"
-import { onMount } from "svelte"
-import { version } from "../../package.json"
-
-onMount(() => {
-	console.log(`App version: ${version}`)
-})
 </script>
 
 <slot />
