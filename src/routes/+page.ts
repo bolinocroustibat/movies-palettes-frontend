78a1c3f8a9fd8faa1e238f5d7b87006472ba592b
Adrien Carpentier <me@adriencarpentier.com>
2024-12-12 11:34:14 +0100

    feat: display version in console

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,10 @@ import { fetchMovies } from "$lib/api"
 import { error } from "@sveltejs/kit"
 import type { PageLoad } from "./$types"
 
+const appVersion: string = __APP_VERSION__
+
 export const load: PageLoad = async ({ fetch }) => {
+	console.log(appVersion)
 	try {
 		const movies = await fetchMovies(fetch)
 		return {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import packageJson from "./package.json"
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		__APP_VERSION__: JSON.stringify(packageJson.version),
+	},
 })
