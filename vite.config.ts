bed272c34b0470c8587685150daadfb407cf8c61
Adrien Carpentier <me@adriencarpentier.com>
2025-01-03 17:45:40 +0100

    feat: also works with a local movies_palettes.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { defineConfig, searchForWorkspaceRoot } from "vite"
 import packageJson from "./package.json"
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
 		__APP_VERSION__: JSON.stringify(packageJson.version),
+	},
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd()),
+				// your custom rules
+				"/Users/bolino/code/perso/movies-palettes-frontend/static/",
+			],
+		},
 	},
 })
