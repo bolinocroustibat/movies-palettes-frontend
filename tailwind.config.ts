05ff10356f432d4963b33c7597a36b189095c8b5
Adrien Carpentier <me@adriencarpentier.com>
2025-01-21 10:18:43 +0900

    clean: remove tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,20 @@ export default {
 					},
 				],
 			},
+			keyframes: {
+				"fade-in": {
+					"0%": { opacity: "0" },
+					"100%": { opacity: "1" },
+				},
+				"slide-down": {
+					"0%": { transform: "translateY(-10px)", opacity: "0" },
+					"100%": { transform: "translateY(0)", opacity: "1" },
+				},
+			},
+			animation: {
+				"fade-in": "fade-in 0.8s ease-out",
+				"slide-down": "slide-down 0.8s ease-out",
+			},
 		},
 	},
 
